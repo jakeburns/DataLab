ef899c74137020df9d523daad2182e3be4d25e08
jakeburns <jakeburns@MacBook-Pro-4.local>
2019-03-06 19:00:57 -0500

    updated

--- colors.js
+++ colors.js
@@ -1,10 +1,37 @@
+var dataCSV = d3.csv("colors.csv");
+var dataJSON = d3.json("colors.json");
 
 
-var drawChart = function(data){
+
+dataJSON.then(function(data)
+{
+  console.log("data",data);
+  drawChart(data, "#json");
+  makeLegend(data, "#legendJ");
+},
+function(err)
+{
+  console.log(err);
+});
+
+dataCSV.then(function(dataSecond)
+{
+  console.log("data",dataSecond);
+  drawChart(dataSecond, "#csv");
+  makeLegend(dataSecond, "#legendCSV");
+},
+function(err)
+{
+  console.log(err);
+});
+
+
+
+var drawChart = function(data, name){
   var height = 200;
   var width = 400;
   var barWidth = width/data.length
-  var svg = d3.select("#json svg")
+  var svg = d3.select(name)
             .attr("width", width)
             .attr("height", height);
 
@@ -21,9 +48,7 @@ var drawChart = function(data){
             .attr("height", function(d){
               return d.num * 10;
             })
-            .attr("width", function(d){
-              return barWidth;
-            })
+            .attr("width", barWidth)
             .attr("fill", function(d){
               return d.color;
             });
@@ -49,15 +74,39 @@ var drawChart = function(data){
 
 
 
-          var dataP = d3.json("colors.json");
 
-          dataP.then(function(data){
 
-            console.log("data", data)
-            drawChart(data);
 
-          },
-          function(err){
-            console.log(err);
-          }
-          );
+var makeLegend = function(theData, name){
+  var width = 200;
+  var height = 200;
+  var boxWidth = 10;
+  var svg = d3.select(name)
+              .attr("height", height)
+              .attr("width", width);
+  svg.selectAll("rect")
+     .data(theData)
+     .enter()
+     .append("rect")
+     .attr("x", function(d,i)
+      { return 21;})
+    .attr("y", function (d, i)
+      { return (i+1)*15 + 9;})
+    .attr("width", boxWidth)
+    .attr("height", boxWidth-2)
+    .attr("fill", function(d)
+      { return d.color;})
+
+svg.selectAll("text")
+   .data(theData)
+   .enter()
+   .append("text")
+   .text(function(d)
+      { return d.color;})
+   .attr("x", function(d,i)
+      { return 45})
+   .attr("y", function(d, i)
+      { return (i+1)*15 + 19;})
+   .attr("fill", "black")
+
+}
